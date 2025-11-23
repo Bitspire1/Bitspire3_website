e53f3147d28a2184b0eaf3c1286372ba4067f249
Bitspire1 <Bitspireone@proton.me>
2025-11-23 17:20:34 +0100

    Update TinaCMS generated schema files for TinaCloud synchronization

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/kubap/Desktop/bitspie_v4 — kopia/frontend/tina/__generated__/.cache/1763913135327', url: 'https://content.tinajs.io/1.6/content/a40ff8b9-0082-4d76-bcf4-65199fb84432/github/main', token: '8498b0864d915e53c11e066762bb90a6c122c739', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/kubap/Desktop/bitspie_v4 — kopia/frontend/tina/__generated__/.cache/1763914757197', url: 'https://content.tinajs.io/1.6/content/a40ff8b9-0082-4d76-bcf4-65199fb84432/github/main', token: '8498b0864d915e53c11e066762bb90a6c122c739', queries,  });
 export default client;
   
