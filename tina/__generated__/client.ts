6f9d1629978887ab92b82cacb4a6c82173c70822
Bitspire1 <Bitspireone@proton.me>
2025-12-11 09:12:52 +0100

    Update Tina generated client after removing portfolioHighlights schema

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/kubap/Desktop/Bitspire kurwa nowe i chuj/tina/__generated__/.cache/1765406531102', url: 'https://content.tinajs.io/2.0/content/f529e4e6-9630-4638-8404-25897477e309/github/rozpierdol', token: 'c0dae5149a6de428b239c63fd03821589f538a6c', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/kubap/Desktop/Bitspire kurwa nowe i chuj/tina/__generated__/.cache/1765440739799', url: 'https://content.tinajs.io/2.0/content/f529e4e6-9630-4638-8404-25897477e309/github/rozpierdol', token: 'c0dae5149a6de428b239c63fd03821589f538a6c', queries,  });
 export default client;
   
